a732ca54e6ce676f1f4c1c58372850b8eb193cbe
kim <s158614@nishitech.ac.jp>
2019-11-11 16:39:02 +0900

    add mongoose db
    db 라이브러리 추가및 db 생성

--- server.js
+++ server.js
@@ -2,16 +2,21 @@
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const productRoutes = require("./routes/api/products");
 const ordersRoutes = require("./routes/api/orders");
 const usersRoutes = require("./routes/api/users");
-//
-// app.use((req, res) => {
-//    res.json({
-//         "msg":"server responsed"
-//    })
-// });
+
+
+//const dbaddress = "mongodb+srv://user1:aa1234@cluster0-nbev1.mongodb.net/test?retryWrites=true&w=majority";
+const dbaddress = "mongodb://teddykwak:k9915402@ds141294.mlab.com:41294/node-rest-shop";
+mongoose.connect(dbaddress, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => console.log("몽고DB connect..."))
+    .catch(err => console.log(err.message));
+
+
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
