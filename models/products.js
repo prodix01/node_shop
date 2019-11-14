7a095d1404a213502355b62884e6847a8105aa6e
kim <s158614@nishitech.ac.jp>
2019-11-14 19:51:43 +0900

    Add order model
    장바구니

--- models/products.js
+++ models/products.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose");
 
 const productSchema = mongoose.Schema({
-    name: String,
-    price: Number
+    name: {
+        type : String,
+        required: true
+    },
+    price: {
+        type : Number,
+        required: true
+    },
+    createdAt: {
+        type : Date,
+        default : Date.now
+    }
 });
 
 
