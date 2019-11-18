3aeb9259138073a67f142690af5ea8256812d778
kim <s158614@nishitech.ac.jp>
2019-11-18 19:52:51 +0900

    email regex

--- models/users.js
+++ models/users.js
@@ -4,7 +4,9 @@ const mongoose = require("mongoose");
 const userSchema = mongoose.Schema({
     email : {
         type : String,
-        required : true
+        required : true,
+        unique : true,
+        match : /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
     },
     password : {
         type : String,
